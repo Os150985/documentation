06e66cbf65375bf830782f18af2c11771791558c
Ozs <moreno150985@gmail.com>
2025-07-20 08:08:51 -0700

    x

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const {resolve} = require('path')
 // XXX do not export a devServer from this file.
 // See https://github.com/advisories/GHSA-wr3j-pwj9-hqq6 for more info.
-module.exports = {
+module.exports = {10000
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
     },
     extensions: ['.js'],
   },
-}
+10000}
